9be369765fb0edfd758c8c23db68ba58dcd1216f
sudar3010 <sudar.son048@gmail.com>
2025-08-10 22:44:57 +0530

    updated setting file

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ SYMBOL = "TCS"   # NSE/BSE symbol of your stock
 # API Configuration
 PRICE_API_URL = f"https://indian-stock-exchange-api2.p.rapidapi.com/stock?name={SYMBOL}"
 PRICE_API_HEADERS = {
-    "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY", "701d4da0e2msh9fb6b21538296c5p1e194fjsn9e4ee7a6c936"),
+    "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
     "X-RapidAPI-Host": "indian-stock-exchange-api2.p.rapidapi.com"
 }
 
@@ -15,9 +15,9 @@ NEWS_API_PARAMS = {
     "q": "TCS",
     "language": "en",
     "sortBy": "publishedAt",
-    "apiKey": os.getenv("NEWS_API_KEY", "3ae1e0aeff514f348eb78a8101af020c")
+    "apiKey": os.getenv("NEWS_API_KEY")
 }
 
 # Telegram Configuration
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8108841318:AAE8aoEPqOU6SrwzRvtAjOQAG9AjD2IT2NI")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "583577008")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
