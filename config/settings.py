e98046eb209a1147f17f0e34b24de8bbe3a9e08c
sudar3010 <sudar.son048@gmail.com>
2025-08-11 13:31:00 +0530

    modified settings

--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,5 @@
 import os
 
-# Stock Configuration
-SYMBOL = "TCS"   # NSE/BSE symbol of your stock
-
-# API Configuration
-PRICE_API_URL = f"https://indian-stock-exchange-api2.p.rapidapi.com/stock?name={SYMBOL}"
 PRICE_API_HEADERS = {
     "X-RapidAPI-Key": os.getenv("RAPIDAPI_KEY"),
     "X-RapidAPI-Host": "indian-stock-exchange-api2.p.rapidapi.com"
